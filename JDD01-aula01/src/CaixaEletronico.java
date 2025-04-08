bc67ce59d8480c8dd215ec4a92de1a7e0bc45f9a
Beatriz <derie.bs@gmail.com>
2025-04-07 21:59:34 -0300

    introdução à aula 2 com estrutura de decisão (if)

--- JDD01-aula01/src/CaixaEletronico.java
+++ JDD01-aula01/src/CaixaEletronico.java
@@ -3,11 +3,17 @@ import java.util.Scanner;
 public class CaixaEletronico {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        double valor;
+        int valor, notas50, notas20, notas10;
         System.out.println("Digite o valor do saque: R$ ");
-        valor = leitor.nextDouble();
+        valor = leitor.nextInt();
         // Cédulas 50, 20, 10
-
-
+        notas50 = valor / 50;
+        System.out.println("Notas de R$50,00: " + notas50);
+        valor = valor % 50;
+        notas20 = valor / 20;
+        System.out.println("Notas de R$20,00: " + notas20);
+        valor %= 20; //valor = valor % 20;
+        notas10 = valor / 10;
+        System.out.println("Notas de R$10,00: " + notas10);
     }
 }

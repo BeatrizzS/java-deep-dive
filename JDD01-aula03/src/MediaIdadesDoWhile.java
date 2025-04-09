c378c5fb3155fd2863581af54341b1c3161ad41e
Beatriz <derie.bs@gmail.com>
2025-04-09 20:52:02 -0300

    arrumando o do..while

--- JDD01-aula03/src/MediaIdadesDoWhile.java
+++ JDD01-aula03/src/MediaIdadesDoWhile.java
@@ -2,23 +2,19 @@ import java.util.Scanner;
 
 public class MediaIdadesDoWhile {
     public static void main(String[] args) {
-
         Scanner leitor = new Scanner(System.in);
-
         int idade = 0, media, numeroAluno;
-        //inicializando a variavel
+        //inicializando a variável da condição
         numeroAluno = 1;
         do {
             System.out.println("Digite a idade do " + numeroAluno + "º aluno: ");
-            //incremento - alterar a variavel idade
             idade = idade + leitor.nextInt();
-        }while (numeroAluno <= 5);{
-                //a condição se torne falta
-                numeroAluno++;
-            }
-
-            media = idade / 5;
-            System.out.println("A média de idade dos alunos da escola é: " + media);
+            //Incremento - alterar a variável da condição para que em algum momento
+            //a condição se torne falsa
+            numeroAluno++;
+        } while (numeroAluno <= 5);//Definir a condição de repetição
+        media = idade / 5;
+        System.out.println("A média de idade dos alunos da escola é: " + media);
 
 
     }
